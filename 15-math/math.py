2b0ec72fb060c6e46e608c5e82a9dbeb080a4337
Anzar Khan <67134357+benzocv@users.noreply.github.com>
2023-11-18 18:50:11 +0530

    now json completed

---
+++ 15-math/math.py
@@ -0,0 +1,14 @@
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+
+print(x)
+print(y)
+
+# abs()	Returns the absolute value of a number
+x = abs(-7.25)
+print(x)
+
+
+
+x = pow(4, 3)
+print(x)
---
+++ 16-json/json-test.py
@@ -0,0 +1,49 @@
+import json
+
+
+x = '{ "name":"John", "age":30, "city":"New York"}'
+y = json.loads(x)
+print(y["age"])
+
+
+
+# Convert from Python to JSON
+x = {
+  "name": "John",
+  "age": 30,
+  "city": "New York"
+}
+y = json.dumps(x)
+print(y)
+
+
+
+# Convert Python objects into JSON strings
+print(json.dumps({"name": "John", "age": 30}))
+print(json.dumps(["apple", "bananas"]))
+print(json.dumps(("apple", "bananas")))
+print(json.dumps("hello"))
+print(json.dumps(42))
+print(json.dumps(31.76))
+print(json.dumps(True))
+print(json.dumps(False))
+print(json.dumps(None))
+
+
+x = {
+  "name": "John",
+  "age": 30,
+  "married": True,
+  "divorced": False,
+  "children": ("Ann","Billy"),
+  "pets": None,
+  "cars": [
+    {"model": "BMW 230", "mpg": 27.5},
+    {"model": "Ford Edge", "mpg": 24.1}
+  ]
+}
+
+# use four indents to make it easier to read the result:
+print(json.dumps(x, indent=4))
+print(json.dumps(x, indent=4, separators=(". ", " = ")))
+print(json.dumps(x, indent=4, sort_keys=True))
